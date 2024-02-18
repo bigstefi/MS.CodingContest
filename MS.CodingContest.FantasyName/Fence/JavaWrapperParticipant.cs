77383de10347e421c35841c72130cd950dde0f35
bigstefi <bigstefi@yahoo.com>
2024-02-18 11:46:17 +0100

    Adding sample Java wrapper C# class.

---
+++ MS.CodingContest.FantasyName/Fence/JavaWrapperParticipant.cs
@@ -0,0 +1,115 @@
+﻿using MS.CodingContest.Helpers.Fence;
+using MS.CodingContest.Helpers.Java;
+using MS.CodingContest.Interfaces.Fence;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.CodingContest.FantasyName.Fence
+{
+    public class JavaWrapperParticipant : IFenceBuilderParticipant, IDisposable
+    {
+        private bool _disposed = false;
+        private string _fantasyName = "Java";
+        private double _fenceLength = 4; // dummy initial value
+        private List<Cell> _trees = null;
+        private static JavaNativeInterface _jni = null; // IDisposable
+        private static string _javaClassName = "FenceBuilderParticipantSample";
+        private static Dictionary<string, string> _pairs = new Dictionary<string, string>();
+
+        #region Constructor
+        public /*static*/ JavaWrapperParticipant()
+        {
+            _jni = new JavaNativeInterface();
+
+            // Set the path where the java class is located
+            AddJVMOption("-Djava.class.path", Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "JavaAssemblies"));
+
+            _jni.LoadVM(_pairs, false);
+            _jni.InstantiateJavaObject(_javaClassName);
+        }
+
+        public void Dispose()
+        {
+            if(_disposed)
+            {
+                return;
+            }
+
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // free native resources if there are any.
+            // nothing to be done here
+
+            if(disposing)
+            {
+                // free managed resources
+                if (_jni != null)
+                {
+                    _jni.Dispose();
+                    _jni = null;
+                }
+            }
+
+            _disposed = true;
+        }
+        #endregion
+
+        #region IFenceBuilderParticipant
+        public string FantasyName
+        {
+            get
+            {
+                return _fantasyName;
+            }
+        }
+
+        public double Encircle(IForestProvider forestProvider)
+        {
+            _trees = new List<Cell>(forestProvider.Trees);
+
+            // ToDo: add your implementation here...
+            List<object> olParameters = new List<object>();
+
+            //string msg = Java.CallMethod<int>("AddTwoNumbers", "(IILjava/lang/String;)I", olParameters).ToString();
+
+            Point[] trees = _trees.Select(item => new Point(item.Row, item.Column)).ToArray();
+
+            olParameters.Clear();
+            olParameters.Add(trees);
+
+            double _fenceLength = _jni.CallMethod<double>("Encircle", "([ILjava/lang/Integer;)I", olParameters);
+
+            return _fenceLength;
+        }
+
+        public List<int> ShowSolution()
+        {
+            int[] treeIndexes = _jni.CallMethod<int[]>("ShowSolution", "([ILjava/lang/Void;)I", null);
+
+            return new List<int>(treeIndexes);
+        }
+        #endregion
+
+        #region Helpers
+        private static void AddJVMOption(string name, string value)
+        {
+            if (!_pairs.ContainsKey(name))
+                _pairs.Add(name, value);
+
+            //   jvmOptions.Add(name + "=" + value);
+        }
+        #endregion
+    }
+}
--- MS.CodingContest/Helpers/Java/JavaNativeInterface.cs
+++ MS.CodingContest/Helpers/Java/JavaNativeInterface.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MS.CodingContest.Helpers.Java
 {
-    public class JavaNativeInterface
+    public class JavaNativeInterface : IDisposable
     {
         private static readonly string JRE_REGISTRY_KEY = Registry.LocalMachine.Name + @"\SOFTWARE\JavaSoft\Java Runtime Environment";
         private static readonly string JRE_DEFAULT_VERSION = "1.8";
@@ -19,6 +20,51 @@ namespace MS.CodingContest.Helpers.Java
         private string javaClassName;
         private JavaVM jvm;
         private JNIEnv env;
+
+        #region Constructor
+        ~JavaNativeInterface()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // free native resources if there are any.
+            if (javaClass != IntPtr.Zero)
+            {
+                env.DeleteGlobalRef(javaClass);
+                javaClass = IntPtr.Zero;
+            }
+
+            if (javaObject != IntPtr.Zero)
+            {
+                env.DeleteLocalRef(javaObject);
+                javaObject = IntPtr.Zero;
+            }
+
+            if (disposing)
+            {
+                // free managed resources
+                if (jvm != null)
+                {
+                    jvm.Dispose();
+                    jvm = null;
+                }
+
+                if (env != null)
+                {
+                    env.Dispose();
+                    env = null;
+                }
+            }
+        }
+        #endregion
 
         public bool AttachToCurrentJVMThread { get; set; }
 
@@ -97,6 +143,143 @@ namespace MS.CodingContest.Helpers.Java
             else AttachToCurrentJVM(args);
         }
 
+        public void InstantiateJavaObject(string ClassName)
+        {
+            // need to create class before we can call any methods
+            javaClassName = ClassName;
+            try
+            {
+                javaClass = env.FindClass(javaClassName);
+
+                IntPtr methodId = env.GetMethodId(javaClass, "<init>", "()V");
+                javaObject = env.NewObject(javaClass, methodId, new JValue() { });
+            }
+            catch
+            {
+                throw new Exception(env.CatchJavaException());
+            }
+        }
+
+        public T CallMethod<T>(string methodName, string sig, List<object> param)
+        {
+            IntPtr methodId = env.GetMethodId(javaClass, methodName, sig);
+            try
+            {
+                if (typeof(T) == typeof(byte))
+                {
+                    // Call the byte method 
+                    byte res = env.CallByteMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(bool))
+                {
+                    // Call the boolean method 
+                    bool res = env.CallBooleanMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                if (typeof(T) == typeof(char))
+                {
+                    // Call the char method 
+                    char res = env.CallCharMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(short))
+                {
+                    // Call the short method 
+                    short res = env.CallShortMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(int))
+                {
+                    // Call the int method               
+                    int res = env.CallIntMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(long))
+                {
+                    // Call the long method 
+                    long res = env.CallLongMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(float))
+                {
+                    // Call the float method 
+                    float res = env.CallFloatMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(double))
+                {
+                    // Call the double method 
+                    double res = env.CallDoubleMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res; // need to fix this
+                }
+                else if (typeof(T) == typeof(string))
+                {
+                    // Call the string method 
+                    IntPtr jstr = env.CallObjectMethod(javaObject, methodId, ParseParameters(sig, param));
+
+                    string res = env.JStringToString(jstr);
+                    env.DeleteLocalRef(jstr);
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(byte[]))
+                {
+                    // Call the byte method
+                    IntPtr jobj = env.CallStaticObjectMethod(javaObject, methodId, ParseParameters(sig, param));
+                    if (jobj == IntPtr.Zero)
+                    {
+                        return default(T);
+                    }
+                    byte[] res = env.JStringToByte(jobj);
+                    env.DeleteLocalRef(jobj);
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(string[]))
+                {
+                    // Call the string array method
+                    IntPtr jobj = env.CallObjectMethod(javaObject, methodId, ParseParameters(sig, param));
+                    if (jobj == IntPtr.Zero)
+                    {
+                        return default(T);
+                    }
+
+                    IntPtr[] objArray = env.GetObjectArray(jobj);
+                    string[] res = new string[objArray.Length];
+
+                    for (int i = 0; i < objArray.Length; i++)
+                    {
+                        res[i] = env.JStringToString(objArray[i]);
+                    }
+
+                    env.DeleteLocalRef(jobj);
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(int[]))
+                {
+                    // Call the int array method
+                    IntPtr jobj = env.CallObjectMethod(javaObject, methodId, ParseParameters(sig, param));
+                    if (jobj == IntPtr.Zero)
+                    {
+                        return default(T);
+                    }
+                    int[] res = env.GetIntArray(jobj);
+                    env.DeleteLocalRef(jobj);
+                    return (T)(object)res;
+                }
+                else if (typeof(T) == typeof(IntPtr))
+                {
+                    // Call the object method and deal with whatever comes back in the call code 
+                    IntPtr res = env.CallObjectMethod(javaObject, methodId, ParseParameters(sig, param));
+                    return (T)(object)res;
+                }
+                return default(T);
+            }
+            catch
+            {
+                throw new Exception(env.CatchJavaException());
+            }
+        }
+
         private void AttachToCurrentJVM(JavaVMInitArgs args)
         {
             // This is only required if you want to reuse the same instance of the JVM
@@ -122,6 +305,119 @@ namespace MS.CodingContest.Helpers.Java
                     }
                 }
             }
+        }
+
+        private JValue[] ParseParameters(string sig, List<object> param)
+        {
+            JValue[] retval = new JValue[param.Count];
+
+            int startIndex = sig.IndexOf('(') + 1;
+
+            for (int i = 0; i < param.Count; i++)
+            {
+                string paramSig = "";
+                if (sig.Substring(startIndex, 1) == "[")
+                    paramSig = sig.Substring(startIndex++, 1);
+
+                if (sig.Substring(startIndex, 1) == "L")
+                {
+                    paramSig = paramSig + sig.Substring(startIndex, sig.IndexOf(';', startIndex) - startIndex);
+                    startIndex++; // skip past ;
+                }
+                else
+                    paramSig = paramSig + sig.Substring(startIndex, 1);
+
+                startIndex = startIndex + (paramSig.Length - (paramSig.IndexOf("[", StringComparison.Ordinal) + 1));
+
+                if (param[i] is string)
+                {
+                    if (!paramSig.Equals("Ljava/lang/String"))
+                    {
+                        throw new Exception("Signature (" + paramSig + ") does not match parameter value (" + param[i].GetType().ToString() + ").");
+                    }
+                    retval[i] = new JValue() { l = env.NewString(param[i].ToString(), param[i].ToString().Length) };
+                }
+                else if (param[i] == null)
+                {
+                    retval[i] = new JValue(); // Just leave as default value
+                }
+                else if (paramSig.StartsWith("["))
+                {
+                    retval[i] = ProcessArrayType(paramSig, param[i]);
+                }
+                else
+                {
+                    retval[i] = new JValue();
+                    FieldInfo paramField = retval[i].GetType().GetFields(BindingFlags.Public | BindingFlags.Instance).AsQueryable().FirstOrDefault(a => a.Name.ToUpper().Equals(paramSig));
+                    if ((paramField != null) && ((param[i].GetType() == paramField.FieldType) || ((paramField.FieldType == typeof(bool)) && (param[i] is byte))))
+                    {
+                        paramField.SetValueDirect(__makeref(retval[i]), paramField.FieldType == typeof(bool)  // this is an undocumented feature to set struct fields via reflection
+                                                      ? JavaVM.BooleanToByte((bool)param[i])
+                                                      : param[i]);
+                    }
+                    else throw new Exception("Signature (" + paramSig + ") does not match parameter value (" + param[i].GetType().ToString() + ").");
+                }
+            }
+            return retval;
+        }
+
+        private JValue ProcessArrayType(string paramSig, object param)
+        {
+            IntPtr arrPointer;
+            if (paramSig.Equals("[I"))
+                arrPointer = env.NewIntArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[J"))
+                arrPointer = env.NewLongArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[C"))
+                arrPointer = env.NewCharArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[B"))
+                arrPointer = env.NewByteArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[S"))
+                arrPointer = env.NewShortArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[D"))
+                arrPointer = env.NewDoubleArray(((Array)param).Length, javaClass);
+            else if (paramSig.Equals("[F"))
+                arrPointer = env.NewFloatArray(((Array)param).Length, javaClass);
+            else if (paramSig.Contains("[Ljava/lang/String"))
+            {
+                IntPtr jclass = env.FindClass("Ljava/lang/String;");
+                try
+                {
+                    arrPointer = env.NewObjectArray(((Array)param).Length, jclass, IntPtr.Zero);
+                }
+                finally
+                {
+                    env.DeleteLocalRef(jclass);
+                }
+
+            }
+            else if (paramSig.Contains("[Ljava/lang/"))
+                arrPointer = env.NewObjectArray(((Array)param).Length, javaClass, (IntPtr)param);
+            else
+            {
+                throw new Exception("Signature (" + paramSig + ") does not match parameter value (" +
+                                   param.GetType().ToString() + "). All arrays types should be defined as objects because I do not have enough time to defines every possible array type");
+            }
+
+            if (paramSig.Contains("[Ljava/lang/"))
+            {
+                for (int j = 0; j < ((Array)param).Length; j++)
+                {
+                    object obj = ((Array)param).GetValue(j);
+
+                    if (paramSig.Contains("[Ljava/lang/String"))
+                    {
+                        IntPtr str = env.NewString(obj.ToString(), obj.ToString().Length);
+                        env.SetObjectArrayElement(arrPointer, j, str);
+                    }
+                    else
+                        env.SetObjectArrayElement(arrPointer, j, (IntPtr)obj);
+                }
+            }
+            else
+                env.PackPrimitiveArray<int>((int[])param, arrPointer);
+
+            return new JValue() { l = arrPointer };
         }
     }
 }
